9533071036ee0b668fac25a0777c64c71b4f17b4
Plabon-kundu <129587412+Plabon-kundu@users.noreply.github.com>
2025-10-28 16:28:00 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def calculate(self, num1, operator, num2):
+        if operator == '+':
+            return num1 + num2
+        elif operator == '-':
+            return num1 - num2
+        elif operator == '*':
+            return num1 * num2
+        elif operator == '/':
+            if num2 != 0:
+                return num1 / num2
+            else:
+                return "Error: Division by zero!"
+        else:
+            return "Invalid operator!"
